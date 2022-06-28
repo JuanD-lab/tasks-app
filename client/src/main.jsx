d70ce20768fed4791d0e90497ab67bec6110b8c1
Juan Daniel <yo-juan@outlook.com>
2022-06-28 15:42:24 -0500

    eliminate unresolved dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css';
-import "react-datepicker/dist/react-datepicker.css";
 import 'flowbite';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
